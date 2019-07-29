17ab39202afee395da9042d8963b6688d6e694ba
Kirsten Bell <BellKJ1@cardiff.ac.uk>
2019-07-29 14:54:11 +0100

    Menu
    Play button implemented.

---
+++ Pingpong1/Assets/Scripts/Loadingbar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadingbar : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronosously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronosously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress/ .9f);
+            Debug.Log("progress");
+            yield return null;
+        }
+
+    }
+}
