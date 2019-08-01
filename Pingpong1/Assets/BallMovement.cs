67c711a01fdadf8407e33263ee74ac3eabe29bd8
saj0123 <53175694+saj0123@users.noreply.github.com>
2019-08-01 10:36:24 +0100

    Added Ball and Bat collision
    Ball and bat collision
    AimTarget for ball direction

---
+++ Pingpong1/Assets/BallMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+   
+    public float fowardForce = 100f;
+    // Update is called once per frame
+    void FixedUpdate()
+    { 
+        rb.AddForce(-fowardForce * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Bat")){
+        }
+    }
+  
+}
+
+   
+
+
---
+++ Pingpong1/Assets/BallResetPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallResetPos : MonoBehaviour
+{
+   public Vector3 startPos;
+   private bool stop = false;
+
+   
+
+   void update(){
+       if(stop)
+        return; 
+       startPos = transform.position; 
+   }
+
+   public void Stop(){
+       stop = true;
+       transform.position = startPos;
+   }
+   
+}
