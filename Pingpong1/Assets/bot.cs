aa91d224560b3a68ef8ba815a32c2afe19580f19
saj0123 <53175694+saj0123@users.noreply.github.com>
2019-08-05 11:18:15 +0100

    Added aimTarget Randomiser
    Deleted furniture asset package, position randomiser 4 bot

--- Pingpong1/Assets/bot.cs
+++ Pingpong1/Assets/bot.cs
@@ -11,6 +11,8 @@ public class bot : MonoBehaviour
 
     public float force = 10f;
 
+    public Transform[] targets; 
+
     Vector3 targetPosition; 
     // Start is called before the first frame update
     void Start()
@@ -30,9 +32,16 @@ public class bot : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
 
+    Vector3 PickTarget()
+    {
+        int randomValue = Random.Range(0, targets.Length);
+        return targets[randomValue].position;
+
+    }
+
       private void OnTriggerEnter(Collider other){
         if(other.CompareTag("Ball")){
-            Vector3 dir = aimTarget.position - transform.position;
+            Vector3 dir = PickTarget() - transform.position;
             other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 5, 0);
             Debug.Log("Hit the ball");
         }
