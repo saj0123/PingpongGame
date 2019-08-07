970e9a1db482c690dd9fb26f67b99f7d094c5de8
saj0123 <53175694+saj0123@users.noreply.github.com>
2019-08-07 10:45:30 +0100

    update

---
+++ Pingpong1/Assets/Scripts/Playerlosewall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerlosewall : MonoBehaviour
+{
+    public AudioSource tickSource3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickSource3 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "ball")
+        {
+            tickSource3.Play();
+            Scoresystem.Score -= 1;
+        }
+    }
+}
